e984cfe4e51a3fec4f955334475820b0cab18767
dwinurkolis <117735015+dwinurkolis29@users.noreply.github.com>
2026-02-18 13:34:07 +0700

    fix: add environment planning

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -43,39 +44,39 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDuhjalJBsbZzGA0WoRSMP4iTU68fSJw0M',
-    appId: '1:356333598974:web:f93c335bd6041852d00daf',
-    messagingSenderId: '356333598974',
-    projectId: 'recording-app-877d8',
-    authDomain: 'recording-app-877d8.firebaseapp.com',
-    storageBucket: 'recording-app-877d8.firebasestorage.app',
-    measurementId: 'G-07SXKLR3Z6',
-  );
+  static FirebaseOptions get web => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY']!,
+        appId: dotenv.env['FIREBASE_WEB_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_WEB_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_WEB_PROJECT_ID']!,
+        authDomain: dotenv.env['FIREBASE_WEB_AUTH_DOMAIN']!,
+        storageBucket: dotenv.env['FIREBASE_WEB_STORAGE_BUCKET']!,
+        measurementId: dotenv.env['FIREBASE_WEB_MEASUREMENT_ID'],
+      );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDcjw_0hsRhvQPRmlyKe7R4_cdCl9fJ3t0',
-    appId: '1:356333598974:android:6a6f9af8e3744eaad00daf',
-    messagingSenderId: '356333598974',
-    projectId: 'recording-app-877d8',
-    storageBucket: 'recording-app-877d8.firebasestorage.app',
-  );
+  static FirebaseOptions get android => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET']!,
+      );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAGK9q2YsdYMgBSFk7nX6mldvwV_gr_JZA',
-    appId: '1:356333598974:ios:6128e6fda62bafc2d00daf',
-    messagingSenderId: '356333598974',
-    projectId: 'recording-app-877d8',
-    storageBucket: 'recording-app-877d8.firebasestorage.app',
-    iosBundleId: 'com.example.utsProject',
-  );
+  static FirebaseOptions get ios => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET']!,
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+      );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyAGK9q2YsdYMgBSFk7nX6mldvwV_gr_JZA',
-    appId: '1:356333598974:ios:6128e6fda62bafc2d00daf',
-    messagingSenderId: '356333598974',
-    projectId: 'recording-app-877d8',
-    storageBucket: 'recording-app-877d8.firebasestorage.app',
-    iosBundleId: 'com.example.utsProject',
-  );
+  static FirebaseOptions get macos => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+        appId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET']!,
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:recording_app/core/services/notification_service.dart';
 import 'firebase_options.dart';
 
@@ -8,6 +9,7 @@ import 'main_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
